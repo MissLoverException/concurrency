40cb0a7e99302375dcdbdd40ac0b44e0d61d330a
changhongyuan <changhongyuan@5i5j.com>
2021-12-13 10:58:05 +0800

    ThreadLocal

---
+++ src/main/java/com/missloverexception/concurrency/HttpInterceptor.java
@@ -0,0 +1,33 @@
+package com.missloverexception.concurrency;
+
+import com.missloverexception.concurrency.example.threadLocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("preHandle");
+        return true;
+    }
+
+    /**
+     * 接口请求完毕后，将threadLocal
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        RequestHolder.remove();
+        log.info("afterCompletion");
+        return;
+    }
+}
